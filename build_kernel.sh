3b4f979066dc10d9ea49a6b4c1d10a569de695c8
Leod3c11 <133391551+Leod3c11@users.noreply.github.com>
2025-06-12 15:08:45 -0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,6 +5,23 @@ export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=T
 export ARCH=arm64
 
-make exynos9830-r8slte_defconfig
-make -j16
+# Caminho da toolchain
+export CLANG_PATH=$PWD/toolchain/clang/host/linux-x86/clang-r349610-jopp
+export GCC_PATH=$PWD/toolchain/gcc
 
+# Toolchain
+export CROSS_COMPILE=$GCC_PATH/bin/aarch64-linux-android-
+export CLANG_TRIPLE=aarch64-linux-gnu-
+
+# Suprime erro de extensão do compilador (cgroup)
+export KCFLAGS="-Wno-gnu-variable-sized-type-not-at-end"
+
+# Adiciona Clang no PATH
+export PATH="$CLANG_PATH/bin:$PATH"
+
+make O=out exynos9830-r8slte_defconfig
+make O=out -j$(nproc) \
+  CC=clang \
+  CROSS_COMPILE=$CROSS_COMPILE \
+  CLANG_TRIPLE=$CLANG_TRIPLE \
+  KCFLAGS="$KCFLAGS"
